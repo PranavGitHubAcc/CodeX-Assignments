d8897762464f48b59d9b84a1a686a737363351d2
Pranav Mahajan <pranavmahajan18@gmail.com>
2023-11-19 11:20:37 +0530

    Update BankAccount.java

--- Assignment_3/BankAccount.java
+++ Assignment_3/BankAccount.java
@@ -1,32 +1,79 @@
+import java.util.*;
+
+class Bank {
+    String name;
+    int accno;
+    double p;
+
+    public Bank(String name, int accno, double principal) {
+        this.name = name;
+        this.accno = accno;
+        this.p = principal;
+    }
+
+    public void display() {
+        System.out.println("Customer Name: " + name);
+        System.out.println("Account Number: " + accno);
+    }
+}
+
+class Account extends Bank {
+    double amt;
+
+    public Account(String name, int accno, double principal) {
+        super(name, accno, principal);
+        amt = 0;
+    }
+
+    public void deposit(double amount) {
+        amt = amount;
+        p += amt;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > p) {
+            System.out.println("INSUFFICIENT BALANCE");
+        } else {
+            amt = amount;
+            p -= amt;
+            if (p < 500) {
+                double penalty = (500 - p) / 10;
+                p -= penalty;
+            }
+        }
+
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Principal Amount: " + p);
+    }
+}
+
 public class BankAccount {
-    int ACNumber;
-    double balance;
-
-    public BankAccount() {
-        ACNumber = 10011;
-        balance = 0;
-    }
-
-    public BankAccount(int num) {
-        ACNumber = 10011;
-        balance = num;
-    }
-
-    public BankAccount(int acc, int num) {
-        ACNumber = acc;
-        balance = num;
-    }
-
     public static void main(String[] args) {
-        BankAccount x = new BankAccount();
-        System.out.print("Default: ");
-        System.out.println(x.ACNumber + " " + x.balance);
-        System.out.print("1 Parameter: ");
-        BankAccount y = new BankAccount(100);
-        System.out.println(y.ACNumber + " " + y.balance);
-        System.out.print("2 Parameters: ");
-        BankAccount z = new BankAccount(10101, 200);
-        System.out.println(z.ACNumber+" " +z.balance);
+        
+        Account account = new Account("Agastya", 19365, 36000.0);
+        
+         System.out.println();
+       
+        account.display();
+        System.out.println();
+         System.out.println("************************");
+
+        account.deposit(500.0);
+
+       
+        account.display();
+        System.out.println();
+
+        System.out.println("************************");
+
+      
+        account.withdraw(300.0);
+
+        
+        account.display();
     }
-    
 }
