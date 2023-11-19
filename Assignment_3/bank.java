461b08a24d1c89d8fbd3db04ceda155aa6655923
Pranav Mahajan <pranavmahajan18@gmail.com>
2023-11-19 11:02:58 +0530

    Update bank.java

--- Assignment_3/bank.java
+++ Assignment_3/bank.java
@@ -1,1 +1,55 @@
+class Bank 
+{
+    String name;
+    int accno;
+    double p;
 
+    public Bank(String name, int accno, double principal) 
+    {
+        this.name = name;
+        this.accno = accno;
+        this.p = principal;
+    }
+
+    public void display() 
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Account Number: " + accno);
+        System.out.println("Principal Amount: " + p);
+    }
+}
+
+class Account extends Bank 
+{
+    public Account(String name, int accno, double principal) 
+    {
+        super(name, accno, principal);
+    }
+
+    public void deposit(double amt) 
+    {
+        p += amt;
+    }
+
+    public void withdraw(double amt) 
+    {
+        if (amt > p) 
+        {
+            System.out.println("INSUFFICIENT BALANCE");
+        } 
+        else 
+        {
+            p -= amt;
+            if (p < 500) {
+                double penalty = (500 - p) / 10;
+                p -= penalty;
+            }
+        }
+    }
+
+    @Override
+    public void display() 
+    {
+        super.display();
+    }
+}
